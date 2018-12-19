194c30eaa432fa197fb84476c223a2b22b82e658
chiheb <chihebguelmami@gmail.com>
2018-12-19 16:55:34 +0100

    remarque

--- myappBack/server/routes/post.js
+++ myappBack/server/routes/post.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router=express.Router();
+const axios =require ("axios");
+
+const data ='https://jsonplaceholder.typicode.com/posts'
+
 router.get('/',(req,res)=>{
     res.send('POST WORKS');
-
+    axios.get(`${data}`).then(posts=>{
+    	console.log(posts.data);
+    });
 });
 module.exports=router;
---
+++ readme.md
@@ -0,0 +1,2 @@
+#Le frond end marche 
+#dans front end j'ai pas finis la partie ou je doit recupérer les données que l'utulisateur a tapé
